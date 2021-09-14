f953ecf0d0b429e5ecbdf3ebe4693335f10a7199
pritexx <bulavinvlad_2004@mail.ru>
2021-09-14 18:23:19 +0300

    Fixed couple issues

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 def main(token):
     from vkbottle import Bot, load_blueprints_from_package
 
-    token = "fdb90ff2dbec36a79ac5b079f58c8c750a3298246753ae9f5e924b84ba92596c5cdcc8d30a2e9104f9589"
+    # token = ""
 
     bot = Bot(token=token)
 
